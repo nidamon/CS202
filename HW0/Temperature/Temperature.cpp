7acfb621bf9c50404dd69cd9c0095b1bc2ec8fa7
Nathan Damon <nidamon@alaska.edu>
2021-01-21 12:20:49 -0900

    Used istringstream to check argv for a number input.

--- HW0/Temperature/Temperature.cpp
+++ HW0/Temperature/Temperature.cpp
@@ -8,24 +8,39 @@ This program will convert temperature from degrees Fahrenheit to degrees Celsius
 #include <string>
 using std::string;
 #include <iostream>
+#include <sstream>
+using std::istringstream;
+
 
 double cpp_ftoc(const char* str);
 double c_ctof(const char* str);
 
-int main(int argc, const char** argv)
+int main(int argc, const char* argv[])
 {
-	if (argc >= 2 && argv[1] == string("--ftoc"))
+	if (argc >= 2)
 	{
-		double c = cpp_ftoc(argv[2]);
-		printf("In degrees Celcius %f.\n", c);
-	}
+		double value;
+		string str = argv[2];
+		istringstream instream(str);
+		instream >> value;
+		if (instream)
+		{
+			if (argc >= 2 && argv[1] == string("--ftoc"))
+			{
+				double c = cpp_ftoc(argv[2]);
+				printf("In degrees Celcius %f.\n", c);
+			}
 
-	if (argc >= 2 && argv[1] == string("--ctof"))
-	{
-		double f = c_ctof(argv[2]);
-		printf("In degrees Fahrenheit %f.\n", f);
+			if (argc >= 2 && argv[1] == string("--ctof"))
+			{
+				double f = c_ctof(argv[2]);
+				printf("In degrees Fahrenheit %f.\n", f);
+			}
+		}
+		else
+			printf("Must input a numerical value.");
 	}
-}
+};
 
 
 // Converts from degrees Fahrenheit to degrees Celsius (stod)
