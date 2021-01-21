b1608d97b84aeac1ca41b330bda7919e5f559c25
Nathan Damon <nidamon@alaska.edu>
2021-01-20 21:46:52 -0900

    Put the if statments to select the function in main and the code to convert from degrees C to F and F to C.

--- HW0/Temperature/Temperature.cpp
+++ HW0/Temperature/Temperature.cpp
@@ -5,23 +5,36 @@ CS 202
 This program will convert temperature from degrees Fahrenheit to degrees Celsius.
 */
 
+#include <string>
+using std::string;
+
 double cpp_ftoc(const char* str);
 double c_ctof(const char* str);
 
 int main(int argc, const char** argv)
 {
+	if (argc >= 2 && argv[1] == string("--ftoc"))
+	{
+		cpp_ftoc(argv[2]);
+	}
 
+	if (argc >= 2 && argv[1] == string("--ctof"))
+	{
+		c_ctof(argv[2]);
+	}
 }
 
 
 // Converts from degrees Fahrenheit to degrees Celsius (stod)
 double cpp_ftoc(const char* str)
 {
-
+	double celcius = (std::stod(str) - 32) * (5/9);
+	return celcius;
 }
 
 // Converts from degrees Celsius to degrees Fahrenheit (strtod)
 double c_ctof(const char* str)
 {
-
+	double fahrenheit = strtod(str, NULL) * (9/5) + 32;
+	return fahrenheit;
 }
