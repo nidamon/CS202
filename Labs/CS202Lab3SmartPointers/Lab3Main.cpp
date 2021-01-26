4f5d77f13527c22ea38836d784f0636eccc6fc26
Nathan Damon <nidamon@alaska.edu>
2021-01-26 12:20:47 -0900

    Revised the text output and added the code for shared pointers.

--- Labs/CS202Lab3SmartPointers/Lab3Main.cpp
+++ Labs/CS202Lab3SmartPointers/Lab3Main.cpp
@@ -10,25 +10,31 @@ This the third lab of CS 202 that will make use of pointers.
 
 int main()
 {
-	cout << "Raw pointer... " << endl;
+	cout << "Creating raw pointer." << endl;
 	Lab2 lab;
 	Lab2* p = &lab;
 
 	cout << endl;
-	cout << "Unique pointer... " << endl;
+	cout << "Creating unique_ptr." << endl;
 	std::unique_ptr<Lab2> uLab1(new Lab2("Uni"));
 
 	cout << endl;
-	cout << "Created Unique pointer #2" << endl;
-	std::unique_ptr<Lab2> uLab2;
-
-	cout << endl;
-	cout << "Transferring ownership of \"Uni\" to Unique pointer #2" << endl;
-	uLab2 = std::move(uLab1);
+	cout << "Transferring ownership of \"Uni\" to a new unique_ptr." << endl;
+	std::unique_ptr<Lab2> uLab2 = std::move(uLab1);
 
 	cout << endl;
 	cout << "Calling member function \"printStuff\"." << endl;
 	uLab2->printStuff();
+
+	cout << endl;
+	cout << "Creating shared_ptr." << endl;
+	std::shared_ptr<Lab2> sLab1(new Lab2("Duo"));
+
+	cout << endl;
+	cout << "Creating another shared_ptr to the same object." << endl;
+	std::shared_ptr<Lab2> sLab2(sLab1);
+
+
 
 	cout << endl;
 }
