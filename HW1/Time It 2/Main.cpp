622ddbd8515cb6553ba3e9035a33cf83b1cb8ff6
Nathan Damon <nidamon@alaska.edu>
2021-02-04 13:53:50 -0900

    Added five text documents of project guten berg books and wrote the code to open and read the first 20 lines of "The Strange Case of Dr. Jekyll..." and print them out.

--- HW1/Time It 2/Main.cpp
+++ HW1/Time It 2/Main.cpp
@@ -5,10 +5,65 @@ CS 202
 This the file that will contain main() for the Time It II program.
 */
 
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+#include <string>
+using std::string;
+#include <sstream>
+using std::istringstream;
+using std::getline;
+#include <fstream>
+using std::ifstream;
+using std::ios;
+
+#include <vector>
+using std::vector;
+#include <list>
+using std::list;
+#include <deque>
+using std::deque;
+	
 #include "../Time It 1/StopWatch.h"
 
 int main()
 {
-	StopWatch Timer;
+	//StopWatch Timer;
+	//Timer.Start();
+	ifstream fin("The Strange Case Of Dr. Jekyll And Mr. Hyde, by Robert Louis Stevenson.txt");
+	if (!fin)
+	{
+		cout << "Did not open the file" << endl;
+		return 0;
+	}
+	else
+	{
+		int count = 20;
+		while (true)
+		{
+			string line;
+			getline(fin, line);
+			if (!fin)
+			{
+				if (fin.eof())
+				{
+					cout << "End of file." << endl;
+					break;
+				}
+				else
+				{
+					cout << "Error reading file." << endl;
+					break;
+				}
+			}
+			else
+				cout << line << endl;
+			count--;
+			if (count <= 0)
+				break;
+		}
+	}
+
 	return 0;
 }
