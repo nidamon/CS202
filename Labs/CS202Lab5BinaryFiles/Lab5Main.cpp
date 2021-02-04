c5c0acb0c76c42ea5b0b719bce01f00f77b3e84d
Nathan Damon <nidamon@alaska.edu>
2021-02-04 11:01:43 -0900

    Finished Lab 5

--- Labs/CS202Lab5BinaryFiles/Lab5Main.cpp
+++ Labs/CS202Lab5BinaryFiles/Lab5Main.cpp
@@ -5,7 +5,58 @@ CS 202
 This is the main file for lab5.
 */
 
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+#include <fstream>
+using std::ifstream;
+using std::ios;
+
+
 int main()
 {
-	return 0;
+	int i = 0;
+	int numInt = 0;
+	int sum = 0;
+	double average = 0.0;
+	ifstream fin("data.dat", ios::binary | ios::in);
+	if (!fin)
+	{
+		cout << "Did not open the file" << endl;
+		return 0;
+	}
+	else
+	{
+		while (true)
+		{
+			fin.read(reinterpret_cast<char*>(&i), sizeof(i));
+			if (!fin)
+			{
+				if (fin.eof())
+				{
+					cout << "End of file." << endl;
+					break;
+				}
+				else
+				{
+					cout << "Error reading file." << endl;
+					break;
+				}
+			}
+
+			numInt++;
+			sum += i;
+		}
+		average = (double)sum / (double)numInt;
+	}
+
+	cout << "Number of integers: " << numInt << endl;
+	cout << "Sum: " << sum << endl;
+	cout << "Average: " << average << endl;
+
+
+	cout << endl;
+	cout << "Program end";
+	cin >> i;
 }
