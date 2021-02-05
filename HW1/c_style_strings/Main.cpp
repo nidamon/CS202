fb10d8fe033b0c445e33674d7d1a0886f5cb1713
Nathan Damon <nidamon@alaska.edu>
2021-02-04 19:05:10 -0900

    Wrote the code for strdups() which was formerly strdup() in c_style_strings.cpp.

--- HW1/c_style_strings/Main.cpp
+++ HW1/c_style_strings/Main.cpp
@@ -6,8 +6,19 @@ This is the Main.cpp file for the c_style_strings program.
 */
 
 #include "c_style_strings.cpp"
+#include <iostream>
+using std::cout;
+using std::endl;
 
 int main()
 {
+	const char* string = "Happy Program";
+	auto copy = strdups(string);
+
+	cout << string << endl;
+	cout << endl;
+	cout << copy << endl;
+	delete copy;
+
 	return 0;
 }
--- HW1/c_style_strings/c_style_strings.cpp
+++ HW1/c_style_strings/c_style_strings.cpp
@@ -5,14 +5,38 @@ CS 202
 This is the c_style_strings.cpp file for the c_style_strings program.
 */
 
+//#include <iostream>
+//using std::cout;
+//using std::endl;
+
 // Copies a C-style string into memory it allocates dynamically (using new)
-char* strdup(const char*)
+inline char* strdups(const char* s)
 {
+	int index = 0;
+	while (true) // Get the size of the C string
+	{
+		if (s[index] == '\0')
+			break;
+		else
+			index++;
+	}
+	char* duplicate = new char[index + 1];
 
+	index = 0; // Reset the count
+	while (true) // Copy letters to duplicate C string
+	{
+		duplicate[index] = s[index]; // Copy the chars one at a time
+		if (s[index] == '\0')
+			break;
+		else
+			index++;
+	}
+
+	return duplicate;
 }
 
-// Finds the first occurrence of the C-style string x in s.
-char* findx(const char* s, char* x)
-{
-
-}
+//// Finds the first occurrence of the C-style string x in s.
+//char* findx(const char* s, char* x)
+//{
+//	
+//}
