c40d24d5788919d4960b73f2bf891f9f460089ad
Nathan Damon <nidamon@alaska.edu>
2021-02-04 16:31:13 -0900

    Created Main.cpp and c_style_strings.cpp

---
+++ HW1/c_style_strings/Main.cpp
@@ -0,0 +1,11 @@
+/*Main.cpp
+Nathan Damon
+CS 202
+2/4/2021
+This is the Main.cpp file for the c_style_strings program.
+*/
+
+int main()
+{
+	return 0;
+}
---
+++ HW1/c_style_strings/c_style_strings.cpp
@@ -0,0 +1,8 @@
+/*Main.cpp
+Nathan Damon
+CS 202
+2/4/2021
+This is the c_style_strings.cpp file for the c_style_strings program.
+*/
+
+
