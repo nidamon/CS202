2bb8fb3522ce8e3bd1c6c07da21572392c84b2fa
Nathan Damon <nidamon@alaska.edu>
2021-02-09 12:55:48 -0900

    Created Main.cpp and Test.cpp.

---
+++ HW2/Linked List/Main.cpp
@@ -0,0 +1,9 @@
+/*Main.cpp
+Nathan Damon
+CS 202
+2/9/2021
+This is the main file for Linked List
+*/
+
+#define CATCH_CONFIG_MAIN // Tells catch to provide a main()
+#include "../../Labs/CS202Lab6Catch2/catch.hpp"
---
+++ HW2/Linked List/Test.cpp
@@ -0,0 +1,6 @@
+/*Test.cpp
+Nathan Damon
+CS 202
+2/9/2021
+This is the file that will contain the tests for Linked List
+*/
