8e0f7922cd7bca97a4b1c215be6f16f0f23b22b3
Nathan Damon <nidamon@alaska.edu>
2021-02-09 11:30:16 -0900

    Created the tests for the overloaded operators and finished Lab6.

--- Labs/CS202Lab6Catch2/Tests.cpp
+++ Labs/CS202Lab6Catch2/Tests.cpp
@@ -11,9 +11,10 @@ This is the file that will contain the tests for Lab 6
 bool operator==(const Vector3f& a, const Vector3f& b) { return (a.x == b.x) && (a.y == b.y) && (a.z == b.z); }
 bool operator!=(const Vector3f& a, const Vector3f& b) { return (a.x != b.x) || (a.y != b.y) || (a.z != b.z); }
 
-Vector3f a;
+
 
 TEST_CASE("Vector3f default constructor test", "[Vector3f]") {
+	Vector3f a;
 	REQUIRE(a.x == 0.0f);
 	REQUIRE(a.y == 0.0f);
 	REQUIRE(a.z == 0.0f);
@@ -23,16 +24,23 @@ float tester1 = 12.3f;
 float tester2 = 54.259f;
 float tester3 = 4.332f;
 
-Vector3f b(tester1, tester2, tester3);
-
 TEST_CASE("Vector3f user input constructor test", "[Vector3f]") {
+	Vector3f b(tester1, tester2, tester3);
 	REQUIRE(b.x == tester1);
 	REQUIRE(b.y == tester2);
 	REQUIRE(b.z == tester3);
 }
 
-Vector3f c(b);
-
 TEST_CASE("Vector3f copy constructor test", "[Vector3f]") {
+	Vector3f b(tester1, tester2, tester3);
+	Vector3f c(b);
 	REQUIRE(c == b);
+}
+
+TEST_CASE("Operator test equality", "[Operator]") {
+	REQUIRE(Vector3f(tester3, tester1, tester2) == Vector3f(tester3, tester1, tester2));
+}
+
+TEST_CASE("Operator test inequality", "[Operator]") {
+	REQUIRE(Vector3f(tester3, tester3, tester3) != Vector3f(tester1, tester1, tester1));
 }
