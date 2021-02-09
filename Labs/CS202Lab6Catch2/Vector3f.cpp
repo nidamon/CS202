ff560b9f28d8c9c8c63ff650dcb0cf08ccc99b5f
Nathan Damon <nidamon@alaska.edu>
2021-02-09 10:35:44 -0900

    Added Vector3f.cpp and Vector3f.h

---
+++ Labs/CS202Lab6Catch2/Vector3f.cpp
@@ -0,0 +1,6 @@
+/*Vector3f.cpp
+Nathan Damon
+CS 202
+2/9/2021
+This is the .cpp for the Vector3f class 
+*/
---
+++ Labs/CS202Lab6Catch2/Vector3f.h
@@ -0,0 +1,19 @@
+/*Vector3f.h
+Nathan Damon
+CS 202
+2/9/2021
+This is the header file for the Vector3f class
+*/
+
+#ifndef VECTOR3F_HPP
+#define VECTOR3F_HPP
+
+class Vector3f
+{
+public:
+
+};
+
+
+
+#endif
