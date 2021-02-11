4cb60d5b7eed8b503b02c0476e0ddba62b2d0d87
Nathan Damon <nidamon@alaska.edu>
2021-02-10 20:12:57 -0900

    Added printPointer() to Main.cpp.

--- HW2/Memory Layout/Main.cpp
+++ HW2/Memory Layout/Main.cpp
@@ -5,7 +5,24 @@ CS 202
 This is the main file for Memory Layout
 */
 
+#include <iostream>
+using std::cout;
+using std::endl;
+
+void printPointer(int i);
+
 int main()
 {
+	int x = 5;
+	printPointer(x);
 	return 0;
+}
+
+// Prints the value and location in memory of the variable passed to it
+void printPointer(int i) 
+{
+	int* iptr = &i;
+	// The unary * operator dereferences the pointer
+	std::cout << "i  = " << *iptr << endl;
+	std::cout << "&i = " << iptr << endl;
 }
