0001e8a519db567bbaeab5067a707f8ea67d2b17
Nathan Damon <nidamon@alaska.edu>
2021-02-10 21:14:23 -0900

    Created dataCompare() that compares its input and outputs whether the addresses move up or down in memory in Main.cpp.

--- HW2/Memory Layout/Main.cpp
+++ HW2/Memory Layout/Main.cpp
@@ -8,21 +8,46 @@ This is the main file for Memory Layout
 #include <iostream>
 using std::cout;
 using std::endl;
+#include <string>
+using std::string;
 
-void printPointer(int i);
+void printPointer(int& i);
+void dataCompare(const string& type, int& one, int& two, int& three, int& four);
 
 int main()
 {
-	int x = 5;
-	printPointer(x);
+	// Looking at the static storage
+	static int tic1 = 1;
+	static int tic2 = 2;
+	static int tic3 = 3;
+	static int tic4 = 4;
+	dataCompare("Static", tic1, tic2, tic3, tic4);
+
 	return 0;
 }
 
 // Prints the value and location in memory of the variable passed to it
-void printPointer(int i) 
+void printPointer(int& i) 
 {
 	int* iptr = &i;
 	// The unary * operator dereferences the pointer
 	std::cout << "i  = " << *iptr << endl;
 	std::cout << "&i = " << iptr << endl;
+}
+
+void dataCompare(const string & type, int& one, int& two, int& three, int& four)
+{
+	cout << type << " moves ";
+	if (&one < &two && &two < &three && &three < &four)
+		cout << "in ascending order";
+	else if (&one > & two&&& two > & three&&& three > & four)
+		cout << "in descending order";
+	else
+		cout << "in an unexpected way";
+	cout << " in memory." << endl;
+
+	printPointer(one);
+	printPointer(two);
+	printPointer(three);
+	printPointer(four);
 }
