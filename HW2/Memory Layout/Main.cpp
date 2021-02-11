b230017d256d2bcb05467d20c220cd5b24c9a178
Nathan Damon <nidamon@alaska.edu>
2021-02-10 23:12:58 -0900

    Added code to look at stack and free store memory handling and made some minor changes in Main.cpp.

--- HW2/Memory Layout/Main.cpp
+++ HW2/Memory Layout/Main.cpp
@@ -18,10 +18,32 @@ int main()
 {
 	// Looking at the static storage
 	static int tic1 = 1;
-	static int tic2 = 2;
+	static int tic2 = 12;
 	static int tic3 = 3;
-	static int tic4 = 4;
+	static int tic4 = 14;
 	dataCompare("Static", tic1, tic2, tic3, tic4);
+
+	// Looking at the stack storage
+	int ack1 = 5;
+	int ack2 = 16;
+	int ack3 = 7;
+	int ack4 = 18;
+	dataCompare("Stack", ack1, ack2, ack3, ack4);
+
+	// Looking at the free store / heap
+	auto eStore1 = new int;
+	auto eStore2 = new int;
+	auto eStore3 = new int;
+	auto eStore4 = new int;
+	*eStore1 = 9;
+	*eStore2 = 110;
+	*eStore3 = 11;
+	*eStore4 = 112;
+	dataCompare("Free store / heap", *eStore1, *eStore2, *eStore3, *eStore4);
+	delete eStore1;
+	delete eStore2;
+	delete eStore3;
+	delete eStore4;
 
 	return 0;
 }
@@ -50,4 +72,5 @@ void dataCompare(const string & type, int& one, int& two, int& three, int& four)
 	printPointer(two);
 	printPointer(three);
 	printPointer(four);
+	cout << endl;
 }
