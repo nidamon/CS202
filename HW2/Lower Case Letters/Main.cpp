b7f50fb564a1459483626fbf8542ec83d08c6b23
Nathan Damon <nidamon@alaska.edu>
2021-02-11 18:30:09 -0900

    Added some more strings with uppercase letters that are then changed to lowercase in Main.cpp.

--- HW2/Lower Case Letters/Main.cpp
+++ HW2/Lower Case Letters/Main.cpp
@@ -20,8 +20,32 @@ int main()
 	cout << cString << endl;
 	to_lower(cString);
 	cout << "Modified: " << cString << endl;
-
+	cout << endl;
 	delete cString;
+
+	auto cStr1 = strdups("NO MORE ALL CAPITALIZATIONS!");
+	auto cStr2 = strdups("There is no need for such LOUD YELLING!");
+	auto cStr3 = strdups("i DoN't KnOw HoW tO tYpE wItHoUt AlTeRnAtInG cApS lOcK.");
+
+	cout << cStr1 << endl;
+	to_lower(cStr1);
+	cout << "Modified: " << cStr1 << endl;
+	cout << endl;
+
+	cout << cStr2 << endl;
+	to_lower(cStr2);
+	cout << "Modified: " << cStr2 << endl;
+	cout << endl;
+
+	cout << cStr3 << endl;
+	to_lower(cStr3);
+	cout << "Modified: " << cStr3 << endl;
+	cout << endl;
+
+	delete cStr1;
+	delete cStr2;
+	delete cStr3;
+
 	return 0;
 }
 
