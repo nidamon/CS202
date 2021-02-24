5b92d48560ef2d8ee3ab58c5a261d4e6a50c364b
Nathan Damon <nidamon@alaska.edu>
2021-02-23 15:25:32 -0900

    Created Money.cpp and Money.hpp and added moneytest.cpp.

---
+++ HW3/MoneyClass/Money.cpp
@@ -0,0 +1,8 @@
+/*Money.cpp
+Nathan Damon
+2/23/2021
+This is the cpp file for the Money Class
+*/
+
+#include "Money.hpp"
+
---
+++ HW3/MoneyClass/Money.hpp
@@ -0,0 +1,20 @@
+/*Money.hpp
+Nathan Damon
+2/23/2021
+This is the header file for the Money Class
+*/
+
+#ifndef MONEY_HPP
+#define MONEY_HPP
+
+class Money
+{
+public:
+
+private:
+
+};
+
+
+#endif // !MONEY_HPP
+
