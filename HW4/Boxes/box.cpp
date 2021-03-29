57cfc4b8ee517cf5075d8265d505997d5f2179ec
Nathan Damon <nidamon@alaska.edu>
2021-03-29 14:43:26 -0800

    Created box.hpp, box.cpp, and added boxtest3.cpp.

---
+++ HW4/Boxes/box.cpp
@@ -0,0 +1,5 @@
+/*box.cpp
+Nathan Damon
+3/29/2021
+This is the cpp file for the Box Class and it's derivatives
+*/
---
+++ HW4/Boxes/box.hpp
@@ -0,0 +1,5 @@
+/*box.hpp
+Nathan Damon
+3/29/2021
+This is the header file for the Box Class and it's derivatives
+*/
