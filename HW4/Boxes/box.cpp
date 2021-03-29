d805755881b85b158605a55923465f39d28bde0b
Nathan Damon <nidamon@alaska.edu>
2021-03-29 14:52:31 -0800

    Wrote the default and the user width+height specified constructors to box.hpp and box.cpp.

--- HW4/Boxes/box.cpp
+++ HW4/Boxes/box.cpp
@@ -3,3 +3,9 @@ Nathan Damon
 3/29/2021
 This is the cpp file for the Box Class and it's derivatives
 */
+
+#include "box.hpp"
+
+Box::Box() : _width{2}, _height{2} {}
+
+Box::Box(unsigned int w, unsigned int h) : _width{ w }, _height{ h } {}
--- HW4/Boxes/box.hpp
+++ HW4/Boxes/box.hpp
@@ -3,3 +3,37 @@ Nathan Damon
 3/29/2021
 This is the header file for the Box Class and it's derivatives
 */
+
+
+#ifndef BOX_HPP
+#define BOX_HPP
+
+class Box
+{
+	Box();
+	Box(unsigned int w, unsigned int h);
+
+	unsigned int _width;
+	unsigned int _height;
+
+};
+
+class FilledBox : public Box
+{
+
+};
+
+class HollowBox : public Box
+{
+
+};
+
+class CheckeredBox : public Box
+{
+
+};
+
+
+
+#endif // !BOX_HPP
+
