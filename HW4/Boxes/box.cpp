24b2e211b536022d9ba04d38ce4b39fda905b2b9
Nathan Damon <nidamon@alaska.edu>
2021-03-29 17:00:15 -0800

    Added the pure virtual void print() and the pure virtual string type() to box.hpp and added the ostream operator<< to box.hpp and box.cpp. Changed getWidth and getHeight to be const.

--- HW4/Boxes/box.cpp
+++ HW4/Boxes/box.cpp
@@ -6,16 +6,22 @@ This is the cpp file for the Box Class and it's derivatives
 
 #include "box.hpp"
 
+ostream& operator<<(ostream& os, const Box& b)
+{
+	b.print(os);
+	return os;
+}
+
 Box::Box() : _width{2}, _height{2} {}
 
 Box::Box(unsigned int w, unsigned int h) : _width{ w }, _height{ h } {}
 
-unsigned int Box::getWidth()
+unsigned int Box::getWidth() const
 {
 	return _width;
 }
 
-unsigned int Box::getHeight()
+unsigned int Box::getHeight() const
 {
 	return _height;
 }
--- HW4/Boxes/box.hpp
+++ HW4/Boxes/box.hpp
@@ -8,17 +8,25 @@ This is the header file for the Box Class and it's derivatives
 #ifndef BOX_HPP
 #define BOX_HPP
 
+#include <ostream>
+using std::ostream;
+#include <string>
+using std::string;
+
 class Box
 {
 public:
 	Box();
 	Box(unsigned int w, unsigned int h);
 
-	unsigned int getWidth();
-	unsigned int getHeight();
+	unsigned int getWidth() const;
+	unsigned int getHeight() const;
 
 	void setWidth(unsigned int w);
 	void setHeight(unsigned int h);
+
+	virtual void print(ostream& os) const = 0;
+	virtual string type() const = 0;
 
 	unsigned int _width;
 	unsigned int _height;
@@ -40,7 +48,7 @@ class CheckeredBox : public Box
 
 };
 
-
+ostream& operator<<(ostream& os, const Box& b);
 
 #endif // !BOX_HPP
 
