2490d0d0f8b8c8a708b52fd67e9f962e7aea5301
Nathan Damon <nidamon@alaska.edu>
2021-04-17 19:30:08 -0800

    Created myreadwrite.hpp and commented out the rational class testing code in templateTest.cpp for this project.

---
+++ HW5/Template Read and Write/myreadwrite.hpp
@@ -0,0 +1,25 @@
+/*myreadwrite.hpp
+Nathan Damon
+4/17/2021
+This is the hpp file for the Template Read() and Write() program
+*/
+
+#ifndef MYREADWRITE_HPP
+#define MYREADWRITE_HPP
+
+#include <ostream>
+
+template <typename T>
+ void myWrite(std::ofstream& ofs, T read)
+ {
+
+ }
+
+ template <typename T>
+ T myRead(std::ofstream& ofs, T read)
+ {
+
+ }
+
+#endif // !MYREADWRITE_HPP
+
